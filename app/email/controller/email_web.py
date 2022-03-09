0817e0000fbd056b37f73dbd1b82e46ee79e28c6
陳重光 <willy.chen@kkday.com>
2022-03-09 11:35:19 +0800

    Request from URL

--- app/email/controller/email_web.py
+++ app/email/controller/email_web.py
@@ -1,8 +1,12 @@
 from app import app
 from flask import request,render_template,flash,abort,url_for,redirect,session,Flask
+import requests
 
 @app.route("/email_service")
 def email_service():
+    a = requests.get("http://localhost:8888/api_email_sync/test1")
+    print(a)
+    print(a.text)
     return "Inside of email_service"
 
 @app.route("/email_service/redirect_from_api")
